c6a47a71568bfad3fed94dc2c83dbe3a0eeef4c4
Huda Yousif <hjoski@gmail.com>
2020-01-14 13:43:37 -0800

    created registrations

---
+++ app/controllers/registerations_controller.rb
@@ -0,0 +1,13 @@
+class RegisterationsController < ApplicationController
+
+    private
+
+    def sign_up_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+
+end
---
+++ test/controllers/registerations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RegisterationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
