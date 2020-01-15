7034c55fb46477215ed88cc04c763bfa0913aa31
Huda Yousif <hjoski@gmail.com>
2020-01-15 15:49:41 -0800

    add in !

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :ingredients, through: :recipes
   has_many :comments 
 
-  # validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: true, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
