4e6d52ec767fddb36955db2cd43c6f3597bf2086
Huda Yousif <hjoski@gmail.com>
2020-01-15 11:27:58 -0800

    add omniauth to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_222526) do
+ActiveRecord::Schema.define(version: 2020_01_15_190314) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_01_14_222526) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
