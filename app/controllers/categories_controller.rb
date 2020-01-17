a6be82c7e8d86da77099ebfae3e8e2b89a979d88
Huda Yousif <hjoski@gmail.com>
2020-01-16 23:28:07 -0800

    created category controller and views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,12 @@
 class CategoriesController < ApplicationController
+
+    def show 
+        @category = Category.find(params[:id])
+        @recipes = @category.recipes
+    end 
+
+    def index
+        @category = current_user.category
+    end 
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
     has_many :recipes
     has_many :users, through: :recipes
 
+    validates :name, uniqueness: true
 end
